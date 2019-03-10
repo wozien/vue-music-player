3e8ef16c357fc0ccce9e3533563d9528f2df1178
wozien <571455773@qq.com>
2019-03-10 16:45:12 +0800

    finish recommend

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading">
+    <img src="./loading.gif">
+    <p class="desc">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+  props: {
+    title: {
+      type: String,
+      default: '正在载入...'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+
+.loading
+  width 100%
+  text-align center
+  img
+    width 24px
+    height 24px
+  .desc
+    line-height 20px
+    font-size $font-size-small
+    color $color-text-l
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,37 +1,43 @@
 <template>
-  <div class="recommend">
-    <div>
-      <div class="slider-wrapper" v-if="recommends.length">
-        <slider>
-          <div v-for="item of recommends" :key="item.id">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </slider>
-      </div>
-
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-          <li class="item" v-for="item of discList" :key="item.dissid">
-            <div class="icon">
-              <img :src="item.imgurl">
+  <div class="recommend" ref="recommend">
+    <scroll class="recommend-content" :data="discList">
+      <div>
+        <div class="slider-wrapper" v-if="recommends.length">
+          <slider>
+            <div v-for="item of recommends" :key="item.id">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl">
+              </a>
             </div>
-            <div class="text">
-              <h2 class="name" v-html="item.creator.name"></h2>
-              <p class="desc" v-html="item.dissname"></p>
-            </div>
-          </li>
-        </ul>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="item of discList" :key="item.dissid">
+              <div class="icon">
+                <img v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import { getRecommend, getDiscList } from 'api/recommend'
 import Slider from 'base/slider/slider'
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import {ERR_OK} from 'api/config'
 
 export default {
@@ -45,6 +51,7 @@ export default {
   created () {
     // 获取推荐的轮播数据
     this._getRecommend()
+    // 获取热门歌单
     this._getDiscList()
   },
   methods: {
@@ -64,7 +71,9 @@ export default {
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll,
+    Loading
   }
 }
 </script>
@@ -73,37 +82,51 @@ export default {
 @import "~common/stylus/variable"
 
 .recommend
-  .slider-wrapper
-    width 100%
+  position fixed
+  top 88px
+  bottom 0
+  width 100%
+  .recommend-content
+    height 100%
     overflow hidden
-  .recommend-list
-    .list-title
-      height 65px
-      line-height 65px
-      text-align center
-      font-size $font-size-medium
-      color $color-theme
-    .item
-      display flex
-      box-sizing border-box
-      padding 0 20px 20px 20px
-      .icon
-        width 60px
-        padding-right 20px
-        img
-          width 60px
-          height 60px
-      .text
-        display flex
-        flex-direction column
-        justify-content center
-        flex 1
-        line-height 20px
-        overflow hidden
+    .slider-wrapper
+      width 100%
+      overflow hidden
+    .recommend-list
+      .list-title
+        height 65px
+        line-height 65px
+        text-align center
         font-size $font-size-medium
-        .name
-          margin-bottom 10px
-          color $color-text
-        .desc
-          color $color-text-d
+        color $color-theme
+      .item
+        display flex
+        box-sizing border-box
+        padding 0 20px 20px 20px
+        .icon
+          width 60px
+          padding-right 20px
+          img
+            width 60px
+            height 60px
+        .text
+          display flex
+          flex-direction column
+          justify-content center
+          flex 1
+          line-height 20px
+          overflow hidden
+          font-size $font-size-medium
+          .name
+            margin-bottom 10px
+            color $color-text
+          .desc
+            color $color-text-d
+
+    .loading-container
+      position absolute
+      top 50%
+      transform translateY(-50%)
+      width 100%
+
 </style>
