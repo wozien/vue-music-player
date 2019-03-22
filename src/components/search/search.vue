1189334a2a54ba65522069473ae456c559a87bf8
wozien <571455773@qq.com>
2019-03-22 22:53:19 +0800

    search box

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,13 +1,85 @@
 <template>
   <div class="search">
-    search
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" v-for="item in hotkey" :key="item.n"
+                  @click="setQuery(item.k)">
+                <span>{{ item.k }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchBox from 'base/search-box/search-box'
+import { getHotKey } from 'api/search'
+import { ERR_OK } from 'api/config'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  data() {
+    return {
+      hotkey: []
+    }
+  },
+  created() {
+    this._getHotKey()
+  },
+  methods: {
+    setQuery(query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    _getHotKey() {
+      getHotKey().then((res) => {
+        if (res.code === ERR_OK) {
+          this.hotkey = res.data.hotkey
+        }
+      })
+    }
+  },
+  components: {
+    SearchBox
+  }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.search
+  .search-box-wrapper
+    margin 20px
+  .shortcut-wrapper
+    position fixed
+    top 178px
+    bottom 0
+    width 100%
+    .shortcut
+      height 100%
+      .hot-key
+        margin 0 20px 20px 20px
+        .title
+          margin-bottom 20px
+          font-size $font-size-medium
+          color $color-text-l
+        .item
+          display inline-block
+          padding 5px 10px
+          margin 0 20px 10px 0
+          border-radius 6px
+          background $color-highlight-background
+          font-size $font-size-medium
+          color $color-text-d
+</style>
