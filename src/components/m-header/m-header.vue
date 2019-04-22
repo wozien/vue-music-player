baeca2ab1919de1c34b1c7337df68bad7f5532b1
wozien <571455773@qq.com>
2019-04-22 23:43:27 +0800

    add user-center

--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -2,6 +2,9 @@
     <div class="m-header">
         <div class="icon"></div>
         <h1 class="text">Chicken Music</h1>
+        <router-link to="/user" tag="div" class="mine">
+            <i class="icon-mine"></i>
+        </router-link>
     </div>
 </template>
 
@@ -34,4 +37,13 @@ export default {
         line-height 44px
         font-size $font-size-large
         vertical-align top
+    .mine
+        position absolute
+        top 0
+        right 0
+        .icon-mine
+            display: block
+            padding: 12px
+            font-size: 20px
+            color: $color-theme
 </style>
