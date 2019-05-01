5e71e94458a5766a9ed31d774706550f882ba8b5
wozien <571455773@qq.com>
2019-05-01 23:14:44 +0800

    add vconsole

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-// import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-// import fastclick from 'fastclick'
 import VueLazyLoad from 'vue-lazyload'
-
 import 'common/stylus/index.styl'
 
-Vue.config.productionTip = false
+/* eslint-disable no-unused-vars */
+// import VConsole from 'vconsole'
+// var vConsole = new VConsole()
 
-// fastclick.attach(document.body)
+Vue.config.productionTip = false
 
 Vue.use(VueLazyLoad, {
   loading: require('common/image/default.png')
